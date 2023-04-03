6852429a4e2d19b79b85334b839608f56e9d4717
darkropo <rodolfo.noguera@gmail.com>
2023-04-03 09:01:57 -0500

    Update axios.js

--- src/Components/axios/axios.js
+++ src/Components/axios/axios.js
@@ -1,3 +1,3 @@
 import axios from 'axios';
-const instance = axios.create({baseURL: 'http://localhost:3001/'});
+const instance = axios.create({baseURL: 'http://ec2-54-233-123-190.sa-east-1.compute.amazonaws.com:3001/'});
 export default instance
