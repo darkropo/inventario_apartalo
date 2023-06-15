862b7e976b55f64ddb6296e39218c92a96a95be2
Rodolfo <rodolfo.noguera@gmail.com>
2023-06-15 10:28:00 -0500

    adding build folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,46 @@
+const path = require('path');
+
 module.exports = function (webpackEnv) {
-    // ...
-    return {
-      // ...
-      entry: ['babel-polyfill', './src/index.js', 'history'],
-      module: {
-        rules: [
-          {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: [{
-              loader: 'babel-loader'
-            }]
-          },
-          {
-            test: /\.css$/,
-            use: ['style-loader', 'css-loader']
-          },
-          {
-            test: require.resolve('history'),
-            use: [{
-              loader: 'expose-loader?History'
-            }]
-          }
-        ]
-      },
-      resolve: {
-        // ...
-        fallback: {
-          crypto: require.resolve("crypto-browserify"),
-          stream: require.resolve("stream-browserify")
+  return {
+    entry: ['babel-polyfill', './src/index.js', 'history'],
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
+          ]
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: require.resolve('history'),
+          use: [
+            {
+              loader: 'expose-loader',
+              options: 'History'
+            }
+          ]
         }
+      ]
+    },
+    resolve: {
+      fallback: {
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('stream-browserify')
       }
-    };
+    },
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'bundle.js'
+    }
   };
+};
